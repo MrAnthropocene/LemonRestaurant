727cc7e235e36b13c21ef8fd09eff4d3445eaa4c
Jesus Montes <montesj4@uabc.edu.mx>
2025-11-22 13:21:14 -0800

    Initial Commit

---
+++ LemonRestaurant/LemonRestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  LemonRestaurantApp.swift
+//  LemonRestaurant
+//
+//  Created by Saul Herrera on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct LemonRestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LemonRestaurantTests/LemonRestaurantTests.swift
@@ -0,0 +1,17 @@
+//
+//  LemonRestaurantTests.swift
+//  LemonRestaurantTests
+//
+//  Created by Saul Herrera on 11/22/25.
+//
+
+import Testing
+@testable import LemonRestaurant
+
+struct LemonRestaurantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
