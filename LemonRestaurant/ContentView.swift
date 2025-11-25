05676e90da4b4cd4284c2f798cfa1b291ce41226
Jesus Montes <montesj4@uabc.edu.mx>
2025-11-24 16:38:40 -0800

    Assigment 1

--- LemonRestaurant/ContentView.swift
+++ LemonRestaurant/ContentView.swift
@@ -7,15 +7,28 @@
 
 import SwiftUI
 
+ 
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Text("About Little Lemon v2")
+            .font(Font.largeTitle)
+            .foregroundColor(.green)
+            .bold()
+        
+        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ")
+            .multilineTextAlignment(.center)
+            .font(.body)
+            .padding()
+        
+        HStack(spacing: 30){
+            Image(systemName: "fork.knife")
+            Image(systemName: "leaf")
+            Image(systemName: "map")
         }
-        .padding()
+        .font(.system(size: 32))
+        .foregroundColor(.yellow)
+        
     }
 }
 
