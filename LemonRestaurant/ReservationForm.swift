fe86fb9eeae5af8dafb4d8563b803b4b0ac1d74b
Jesus Montes <montesj4@uabc.edu.mx>
2025-11-24 19:45:23 -0800

    Assigment 2

---
+++ LemonRestaurant/ReservationForm.swift
@@ -0,0 +1,18 @@
+//
+//  ReservationForm.swift
+//  LemonRestaurant
+//
+//  Created by Saul Herrera on 11/24/25.
+//
+
+import SwiftUI
+
+struct ReservationForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReservationForm()
+}
